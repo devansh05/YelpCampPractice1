5776e239d426124d710f338770f842dd361f6c6a
devansh2393@gmail.com <devansh2393@gmail.com>
2023-04-27 01:55:02 +0530

    Commit 2 Boiler plate code for mongo db connection

--- app.js
+++ app.js
@@ -1,14 +1,44 @@
+//imports & initializers
 const express = require("express");
 const app = express();
 const path = require("path");
+const mongoose = require("mongoose");
+//Initiating db model
+const Campground = require("./models/campground");
 
+//Initiating db connection
+mongoose
+  .connect("mongodb://127.0.0.1:27017/yelp-camp")
+  .then(() => {
+    console.log("Mongo Connected!");
+  })
+  .catch((error) => {
+    console.log("Mongo Connection Error ", error);
+  });
+
+  //initiating db
+  const db = mongoose.connection;
+  db.on("error", console.log.bind(console, "db connection error:"));
+  db.once("open", () => {
+    console.log('db connected');
+  })
+
+//Setting up EJS
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+//Paths
 app.get('/', (req, res) => {
     res.render('home');
+});
+
+app.get('/makecampground', async (req, res) => {
+    const camp = new Campground({title:'Triund', price:'700', location:'Mclodgeganj', description:'beginners trek, very easy'});
+    await camp.save();
+    res.send(camp);
 })
 
+//Setting up local server
 app.listen(3000, () => {
-    console.log('Serving on 3000')
-})
+    console.log('Serving on 3000');
+});
