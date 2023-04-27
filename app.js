f1d0782349842f90e1d9fc7b5860e52c51615878
devansh2393@gmail.com <devansh2393@gmail.com>
2023-04-27 14:15:49 +0530

    Commit 3 Adding seeds to db

--- app.js
+++ app.js
@@ -17,29 +17,37 @@ mongoose
     console.log("Mongo Connection Error ", error);
   });
 
-  //initiating db
-  const db = mongoose.connection;
-  db.on("error", console.log.bind(console, "db connection error:"));
-  db.once("open", () => {
-    console.log('db connected');
-  })
-
-//Setting up EJS
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
-//Paths
-app.get('/', (req, res) => {
-    res.render('home');
+//initiating db
+const db = mongoose.connection;
+db.on("error", console.log.bind(console, "db connection error:"));
+db.once("open", () => {
+  console.log("db connected");
 });
 
-app.get('/makecampground', async (req, res) => {
-    const camp = new Campground({title:'Triund', price:'700', location:'Mclodgeganj', description:'beginners trek, very easy'});
-    await camp.save();
-    res.send(camp);
-})
+//Setting up EJS
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+//Paths
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/makecampground", async (req, res) => {
+  // const camp = new Campground({
+  //   title: "Triund",
+  //   price: "700",
+  //   location: "Mclodgeganj",
+  //   description: "beginners trek, very easy",
+  // });
+  // await camp.save().then(() => {
+  //   //Closing db connection vvimp
+  //   mongoose.connection.close()
+  // });
+  // res.send(camp);
+});
 
 //Setting up local server
 app.listen(3000, () => {
-    console.log('Serving on 3000');
+  console.log("Serving on 3000");
 });
---
+++ utilities/utility.js
@@ -0,0 +1,3 @@
+module.exports.randomValueFromArray = (array) => array[Math.floor(Math.random() * array.length)];
+
+module.exports.randomFromCount = (count) => Math.floor(Math.random() * count);
